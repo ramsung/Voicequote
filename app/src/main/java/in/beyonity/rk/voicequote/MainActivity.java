dbf78de914fd5f6fddb2c71b8e5217b6a98109e6
mohan <immohanravi@gmail.com>
2018-01-13 21:29:12 +0530

    vg completed

--- app/src/main/java/in/beyonity/rk/voicequote/MainActivity.java
+++ app/src/main/java/in/beyonity/rk/voicequote/MainActivity.java
@@ -2,6 +2,7 @@ package in.beyonity.rk.voicequote;
 
 import android.content.Intent;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -18,29 +19,47 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import in.beyonity.rk.voicequote.adapters.mainPage;
+import in.beyonity.rk.voicequote.fragments.create;
+import in.beyonity.rk.voicequote.fragments.feed;
+import in.beyonity.rk.voicequote.fragments.profile;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    ViewPager vg;
+    create c;
+    feed f;
+    profile p;
+    mainPage adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vg = (ViewPager) findViewById(R.id.vg);
+        adapter = new mainPage(getSupportFragmentManager());
+        c = new create();
+        f = new feed();
+        p = new profile();
+        adapter.addFragment(f);
+        adapter.addFragment(c);
+        adapter.addFragment(p);
+        vg.setAdapter(adapter);
+
+
     }
 
 
     public void prof(View v) {
         Log.i("clicks","You clicked Profile");
-        Intent i=new Intent(
-                MainActivity.this,
-                profile.class);
-        startActivity(i);
+        vg.setCurrentItem(2);
     }
 
-    public void menu(View v) {
+    public void create(View v) {
 
-        Intent ai=new Intent(
-                MainActivity.this,
-                create_quote.class);
-        startActivity(ai);
+        vg.setCurrentItem(1);
+    }
+
+    public void feedClick(View v){
+        vg.setCurrentItem(0);
     }
 }
--- app/src/main/java/in/beyonity/rk/voicequote/fragments/create.java
+++ app/src/main/java/in/beyonity/rk/voicequote/fragments/create.java
@@ -1,41 +1,95 @@
-package in.beyonity.rk.voicequote;
+package in.beyonity.rk.voicequote.fragments;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import in.beyonity.rk.voicequote.R;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
 /**
- * Created by RK on 1/13/2018.
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link create.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link create#newInstance} factory method to
+ * create an instance of this fragment.
  */
+public class create extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
-public class create_quote extends AppCompatActivity {
-
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
     TextView quotetext;
     int maxChar =  2000;
+    View v;
+    private OnFragmentInteractionListener mListener;
+
+    public create() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment create.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static create newInstance(String param1, String param2) {
+        create fragment = new create();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu);
-        quotetext = (TextView) findViewById(R.id.quotetext);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       v = inflater.inflate(R.layout.menu,container,false);
+        quotetext = (TextView) v.findViewById(R.id.quotetext);
 
         quotetext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final LinearLayout mainLayout = (LinearLayout)
-                        findViewById(R.id.main_layout);
+                        v.findViewById(R.id.main_layout);
                 final PopupWindow popupWindow;
                 // inflate the layout of the popup window
                 LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(LAYOUT_INFLATER_SERVICE);
+                        getActivity().getSystemService(LAYOUT_INFLATER_SERVICE);
                 final View popupView = inflater.inflate(R.layout.popup, null);
 
                 String text = quotetext.getText().toString();
@@ -118,5 +172,36 @@ public class create_quote extends AppCompatActivity {
                 });
             }
         });
+        return v;
+
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
