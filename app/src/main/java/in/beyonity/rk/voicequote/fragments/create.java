81e2d6027fb7eb59c778265a9647fd8e310771d6
mohan <immohanravi@gmail.com>
2018-01-17 00:30:23 +0530

    audio recording working

--- app/src/main/java/in/beyonity/rk/voicequote/fragments/create.java
+++ app/src/main/java/in/beyonity/rk/voicequote/fragments/create.java
@@ -2,6 +2,7 @@ package in.beyonity.rk.voicequote.fragments;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
@@ -23,15 +24,18 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import in.beyonity.rk.voicequote.R;
-import in.beyonity.rk.voicequote.RecordingActivity;
 import in.beyonity.rk.voicequote.utils.VisualizerView;
 
+import static android.content.ContentValues.TAG;
 import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 /**
@@ -52,10 +56,14 @@ public class create extends Fragment {
     private String mParam1;
     private String mParam2;
     ImageButton voice;
+    ImageButton play;
+    ImageButton delete;
+    SeekBar seekBar;
     TextView quotetext;
+    boolean paused = false,resumed = false,started = false;
     int maxChar =  2000;
     View v;
-
+    MediaPlayer player;
 
     //recording
     ImageView imageView;
@@ -71,7 +79,10 @@ public class create extends Fragment {
     private boolean isRecording = false;
 
 
-    private Handler handler; // Handler for updating the visualizer
+    private Handler handler;
+    private Handler seekbarHandler = new Handler();
+
+    // Handler for updating the visualizer
     // private boolean recording; // are we currently recording?
 
 
@@ -113,10 +124,20 @@ public class create extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        v = inflater.inflate(R.layout.fragment_create,container,false);
+
        voice = (ImageButton) v.findViewById(R.id.voice);
+
+        handler = new Handler();
        voice.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
+               audioDirTemp = new File(Environment.getExternalStorageDirectory(),
+                       DIRECTORY_NAME_TEMP);
+               if (audioDirTemp.exists()) {
+                   //deleteFilesInDir(audioDirTemp);
+               } else {
+                   audioDirTemp.mkdirs();
+               }
                ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.RECORD_AUDIO},1);
                final LinearLayout mainLayout = (LinearLayout)
                        v.findViewById(R.id.main_layout);
@@ -129,18 +150,109 @@ public class create extends Fragment {
                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
 
-               popupWindow = new PopupWindow(popupView, width, height, focusable);
 
+               popupWindow = new PopupWindow(popupView, width, height, focusable);
+               popupWindow.setOutsideTouchable(false);
                // show the text_popup window
                popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
                visualizerView = (VisualizerView) popupView.findViewById(R.id.visualizer);
                imageView = (ImageView) popupView.findViewById(R.id.RecordButton);
                recordtxt = (TextView) popupView.findViewById(R.id.recordtxt);
 
+
+               play = (ImageButton) popupView.findViewById(R.id.playbutton);
+
+               delete = (ImageButton) popupView.findViewById(R.id.deletebutton);
+
+               seekBar = (SeekBar) popupView.findViewById(R.id.seekBar);
+
+
+               play.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+                       Log.d(TAG, "onClick: inside play");
+                       if(isRecording){
+                           Toast.makeText(getContext(), "Stop the recording", Toast.LENGTH_SHORT).show();
+                           return;
+                       }
+                       File file = new File(audioDirTemp.getPath()+"/"+"audio_file.mp3");
+                       if(file.exists()){
+                            if(player!= null){
+                                Log.d(TAG, "onClick: inside not null");
+                                if(player.isPlaying()){
+                                    Log.d(TAG, "onClick: inside isplayer");
+                                    player.pause();
+                                    paused = true;
+                                    play.setImageResource(R.drawable.play);
+                                }else if(paused){
+                                    player.start();
+                                    paused = false;
+                                    play.setImageResource(R.drawable.ic_pause_black_24dp);
+                                }else {
+                                    Log.d(TAG, "onClick: inside not playing");
+
+                                    play();
+
+                                    try {
+                                        player.setDataSource(file.getPath());
+                                        player.prepareAsync();
+
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }else {
+                                Log.d(TAG, "onClick: inside player null");
+                                play();
+                                try {
+                                    player.setDataSource(file.getPath());
+                                    player.prepareAsync();
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                       }else {
+                           Log.d(TAG, "onClick: file not exist");
+                       }
+                   }
+               });
+
+               delete.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+                       File file = new File(audioDirTemp.getPath()+"/"+"audio_file.mp3");
+                       if(player!=null) {
+                           if (player.isPlaying()||paused) {
+                               Toast.makeText(getContext(), "please stop the player beforing deleting the file", Toast.LENGTH_SHORT).show();
+                               return;
+                           }
+                       }
+                       if(isRecording){
+                           Toast.makeText(getContext(), "please stop the recording beforing deleting the file", Toast.LENGTH_SHORT).show();
+                           return;
+                       }
+                           if (file.exists()) {
+                               file.delete();
+                               Toast.makeText(getContext(), "Deleted Successfully", Toast.LENGTH_SHORT).show();
+                               recordtxt.setText("00:00");
+                           } else {
+                               Toast.makeText(getContext(), "nothing to delete", Toast.LENGTH_SHORT).show();
+                           }
+
+                       }
+               });
                imageView.setOnClickListener(new View.OnClickListener() {
                    @Override
                    public void onClick(View view) {
                        if (!isRecording) {
+                           if(player!=null){
+                               if(player.isPlaying()||paused){
+                                   Toast.makeText(getContext(), "Stop the player before recording", Toast.LENGTH_SHORT).show();
+                                   return;
+                               }
+                           }
                            // isRecording = true;
                            recordtxt.setText("00:00");
                            imageView.setImageResource(R.drawable.ic_mic_red_24dp);
@@ -182,16 +294,50 @@ public class create extends Fragment {
 
                });
 
-               audioDirTemp = new File(Environment.getExternalStorageDirectory(),
-                       DIRECTORY_NAME_TEMP);
-               if (audioDirTemp.exists()) {
-                   deleteFilesInDir(audioDirTemp);
-               } else {
-                   audioDirTemp.mkdirs();
-               }
+
 
                // create the Handler for visualizer update
-               handler = new Handler();
+               popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                   @Override
+                   public void onDismiss() {
+                       Log.d(TAG, "onDismiss: inside on dismiss");
+                       if(isRecording){
+                           releaseRecorder();
+                           Log.d(TAG, "onDismiss: inside on record dismiss");
+                           if(player != null){
+                               if(player.isPlaying()||paused){
+                                   Log.d(TAG, "onDismiss: inside on paused");
+                                   player.stop();
+                                   player.reset();
+                                   paused = false;
+
+                               }
+                               seekBar.setProgress(0);
+
+                           }
+
+
+
+                           handler.removeCallbacks(updateVisualizer);
+                           visualizerView.clear();
+
+                       }
+                       releaseRecorder();
+                       if(player != null){
+                           if(player.isPlaying()||paused){
+                               Log.d(TAG, "onDismiss: inside not recording");
+                               player.stop();
+                               player.reset();
+                               paused = false;
+
+                           }
+                           seekBar.setProgress(0);
+
+                       }
+
+
+                   }
+               });
 
 
            }
@@ -234,6 +380,7 @@ public class create extends Fragment {
 
                 // show the text_popup window
                 popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
+
 
                 // dismiss the text_popup window when touched
                 /*popupView.setOnTouchListener(new View.OnTouchListener() {
@@ -287,9 +434,37 @@ public class create extends Fragment {
 
                     }
                 });
+
             }
         });
         return v;
+
+    }
+
+    public void play(){
+        player = new MediaPlayer();
+        player.reset();
+        paused = false;
+        player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                player.start();
+                seekBar.setMax(player.getDuration());
+                seekbarHandler.post(UpdateSongTime);
+                play.setImageResource(R.drawable.ic_pause_black_24dp);
+            }
+        });
+
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Log.d(TAG, "onCompletion: inside on complete");
+                seekBar.setProgress(0);
+
+                seekbarHandler.removeCallbacks(UpdateSongTime);
+                play.setImageResource(R.drawable.play);
+            }
+        });
 
     }
 
@@ -325,6 +500,7 @@ public class create extends Fragment {
         mListener = null;
     }
 
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -349,6 +525,7 @@ public class create extends Fragment {
             recorder.reset();
             recorder.release();
             recorder = null;
+
         }
     }
 
@@ -364,7 +541,7 @@ public class create extends Fragment {
                 visualizerView.invalidate(); // refresh the VisualizerView
                 timeInMilliseconds = SystemClock.uptimeMillis() - startHTime;
 
-                updatedTime = timeSwapBuff + timeInMilliseconds;
+                updatedTime = timeInMilliseconds;
 
                 int secs = (int) (updatedTime / 1000);
                 int mins = secs / 60;
@@ -409,5 +586,19 @@ public class create extends Fragment {
             // permissions this app might request.
         }
     }
+
+    private Runnable UpdateSongTime = new Runnable() {
+        public void run() {
+           int startTime = player.getCurrentPosition();
+            recordtxt.setText(String.format("%02d:%02d",
+                    TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                    toMinutes((long) startTime)))
+            );
+            seekBar.setProgress((int)startTime);
+            seekbarHandler.postDelayed(this, 100);
+        }
+    };
 
 }
