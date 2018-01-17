7a360bfa973841e76cedeb5d692d89b8f98a49e4
RK <a.r.ramkumar25@gmail.com>
2018-01-17 09:50:09 +0530

    initial

--- app/src/main/java/in/beyonity/rk/voicequote/fragments/create.java
+++ app/src/main/java/in/beyonity/rk/voicequote/fragments/create.java
@@ -117,6 +117,7 @@ public class create extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
